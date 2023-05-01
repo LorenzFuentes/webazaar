2b0abc15c88002d04a344f5548fbad02c414f09b
LorenzFuentes <fuentes2231163@mls.ceu.edu.ph>
2023-05-01 11:31:43 +0800

    Updated homepage(index.php)

--- index.php
+++ index.php
@@ -1,65 +1,259 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Registrar Portal</title>
-  <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap.min.css">
-  <link href="vendor/css/all.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css"  href="resource/css/styles.css">
-</head>
-<body>
-    <header>
-        <nav class="navbar navbar-dark bg-white shadow-sm">
-          <a class="navbar-brand" href="https://malolos.ceu.edu.ph/">
-            <img src="resource/img/logo.jpg" height="70" class="d-inline-block align-top "
-              alt="mdb logo"><h3 class="ib">
-          </a>
-             <a href="https:/www.facebook.com/theCEUofficial/"><i class="fab fa-facebook-f ceucolor"></i></a>
-             <a href="https://www.instagram.com/ceuofficial/"><i class="fab fa-instagram ceucolor"></i></a>
-             <a href="https://twitter.com/ceumalolos"><i class="fab fa-twitter ceucolor"></i></a>
-        </nav>
-        <div class="container-fluid p-0 ">
-            <div class="cover"></div>
-            <video id="videoBG" autoplay muted loop>
-                <source src="resource/mp4/bg.mp4" type="video/mp4">
-                </video>
-            <div class="container bg">
-                <div class="jumbotron">
-                    <h1 class="display-4 text-center bottomline">Office of the University Registrar <span class="ceucolor2">Portal</span></h1>
-                    <p class="text-center">The Centro Escolar University Office of the Registrar supports the college/schools
-and the university in realizing its commitment for the total development of students by providing
-efficient and quality service in terms of registering, updating, evaluating and safekeeping of
-student records, participating in curriculum making/revision and implementing the
-University and CHED policies, rules and regulations.</p>
-                    <p class="lead">
-                    <div class="container-fluid">
-                        <div class="col col-sm-12  text-center mt-5">
-                            <a class="btn btn-outline-light w-50 " href="alumniRegister.php" role="button">I'm an Alumni</a>
-                        </div>
-                        <div class="col col-sm-12  text-center mt-4">
-                            <a class="btn btn-outline-light w-50 " href="login.php" role="button">Log-in</a>
-                        </div>
-                    </div>
-                    </p>
-                </div>
-            </div>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="resource/css/homepage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://kit.fontawesome.com/400dff856d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <title>Webazaar</title>
+  </head>
+  <body>
+
+  <header>
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler text-center" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon  text-center"></span>
+        </button>
+        <div class="collapse navbar-collapse align-items-center justify-content-center" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item pr-3 ">
+              <a class="nav-link text-light" href="#">HOME</a>
+            </li>
+            <li class="nav-item dropdown pr-3 ">
+              <a class="nav-link dropdown-toggle text-light " href="#" role="button" data-toggle="dropdown" aria-expanded="false">SHOP</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">WOMENS</a>
+                <a class="dropdown-item" href="#">MENS</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">ACCESORIES</a>
+              </div>
+            </li>
+            <li class="nav-item pr-3 ">
+              <a class="nav-link text-light " href="#">FEATURES</a>
+            </li>
+            <li class="nav-item pr-3 ">
+              <a class="nav-link text-light ">BLOG</a>
+            </li>
+            <li class="nav-item pr-3 ">
+              <a class="nav-link text-light ">ABOUT</a>
+            </li>
+            <li class="nav-item pr-3 ">
+              <a class="nav-link text-light ">CONTACT</a>
+            </li>
+          </ul>
         </div>
-    </header>
-    <footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom">
-      <div class="container text-center">
-          <div class="row">
-              <div class="col col-sm-5 text-left">
-                  <small>Copyright &copy;Centro Escolar University     Office of the Registrar 2019</small>
-              </div>
-              <div class="col text-right">
-                  <small>Created by: Reymart Bolasoc, Amelia Valencia , James Mangalile, Kenneth De Leon , Pamela Reyes , Ellen Mijares</small>
-              </div>
-          </div>
+      </nav>
+
+    <div class="container">
+      <div class="jumbotron jumbotron-fluid text-center head-set">
+        <div class="container animate__fadeInUp">
+          <h1 class="animate__animated animate__fadeInUp header-name" >WEBAZAAR</h1>
+          <p class="text-light">CLOTHING & ACCESORIES ONLINE SHOP</p>
+          <a class="text-light" href="#categ">  <button class="set"> SHOP NOW</button></a>
+        </div>
       </div>
-    </footer>
-    <script src="vendor/js/jquery.js"></script>
-    <script src="vendor/js/popper.js"></script>
-    <script src="vendor/js/bootstrap.min.js"></script>
-</body>
+    </div>
+  </header>
+
+  <div class="bg-banner" id="categ">
+    <div class="wave">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+      </svg>
+    </div>
+		<div class="container">
+      <h1 class="text-dark text-weight pt-5 mb-3 subhead-set">CATEGORIES</h1>
+      <div class="row p-4">
+        <div class="col-md-6 bg-dark mt-3 bg-set">
+          <a href="product.php" class="text-decoration-none">  <h2 class="linear-wipe text-light text-center">WOMEN</h2></a>
+        </div>
+        <div class="col-md-6 bg-light mt-3 bg-set1">
+            <a href="product.php" class="text-decoration-none">  <h2 class="linear-wipe text-light text-center">MEN</h2></a>
+        </div>
+      </div>
+  	</div>
+
+  <section class="sec-set">
+    <div class="container">
+      <h1 class="mt-5 pt-5 mb-3 pb-2 text-weight">FEATURED</h1>
+      <div class="row">
+        <div class="card-deck">
+          <div class="card">
+            <img src="resource/img/8.2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">WHITE CROP TOP</h5>
+              <p class="card-text">WOMEN | ₱500</p>
+
+            </div>
+            <a href="product.php">
+              <button class="card-button">MORE INFO</button>
+            </a>
+          </div>
+          <div class="card">
+            <img src="resource/img/8.1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">TURTLENECK LONG SLEEVES</h5>
+              <p class="card-text">WOMEN | ₱800</p>
+            </div>
+            <a href="product.php">
+              <button class="card-button">MORE INFO</button>
+            </a>
+          </div>
+          <div class="card">
+            <img src="resource/img/8.3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">COLLARED LONG SLEEVES</h5>
+              <p class="card-text">WOMEN | ₱750</p>
+            </div>
+            <a href="product.php">
+              <button class="card-button">MORE INFO</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-4 mb-5">
+        <div class="card-deck">
+          <div class="card">
+            <img src="resource/img/5.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">WHITE TURTLENECK</h5>
+              <p class="card-text">MEN | ₱750</p>
+            </div>
+            <a href="product.php">
+              <button class="card-button">MORE INFO</button>
+            </a>
+          </div>
+          <div class="card">
+            <img src="resource/img/9.1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">BASIC BLACK TURTLENECK</h5>
+              <p class="card-text">MEN | ₱800</p>
+            </div>
+            <a href="product.php">
+              <button class="card-button">MORE INFO</button>
+            </a>
+          </div>
+          <div class="card">
+            <img src="resource/img/9.2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">BROWN SUIT</h5>
+              <p class="card-text">MEN | ₱800</p>
+            </div>
+            <a href="product.php">
+              <button class="card-button">MORE INFO</button>
+            </a>
+          </div>
+        </div>
+      </div>
+        <div class="row mb-5 pb-5">
+          <div class="card-deck">
+            <div class="card">
+              <img src="resource/img/10.1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">BELT</h5>
+                <p class="card-text">ACCESORIES  </p>
+              </div>
+              <a href="product.php">
+                <button class="card-button">GO TO SHOP</button>
+              </a>
+            </div>
+            <div class="card">
+              <img src="resource/img/10.2.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">WATCH</h5>
+                <p class="card-text">ACCESORIES  </p>
+              </div>
+              <a href="product.php">
+                <button class="card-button">GO TO SHOP</button>
+              </a>
+            </div>
+            <div class="card">
+              <img src="resource/img/10.3.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">BAG</h5>
+                <p class="card-text">ACCESORIES  </p>
+              </div>
+              <a href="product.php">
+                <button class="card-button">GO TO SHOP</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer class="text-center footer text-center">
+    <div class="container">
+        <div class="row text-md-start">
+          <div class="col-md-4">
+            <img src="resource/img/logoweb.png" height="100" width="150" alt="">
+            <p class=" text-center">
+              Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</a>
+            </p>
+          </div>
+          <div class="col-md-3">
+           <h5 class="pb-3">CATEGORIES </h5>
+            <ul class="list-style ps-0 ">
+              <li class="pb-3">WOMEN CLOTHING</li>
+              <li class="pb-3">MEN CLOTHING</li>
+            </ul>
+          </div>
+          <div class="col-md-3 ">
+           <h5 class="pb-3">ACCESORIES </h5>
+            <ul class="list-style ps-0">
+              <li class="pb-3">WATCH</li>
+              <li class="pb-3">BELT</li>
+              <li class="pb-3">BAG</li>
+            </ul>
+          </div>
+          <div class="col-md-2 ">
+           <h5 class="pb-3"> OTHERS </h5>
+            <ul class="list-style ps-0">
+              <li class="pb-3">BLOG</li>
+              <li class="pb-3">FEATURES</li>
+              <li class="pb-3">ABOUT</li>
+              <li class="pb-3">CONTACT</li>
+            </ul>
+          </div>
+        </div>
+    </div>
+    <div class="text-center mb-3">
+       <h5 class=" text-center">
+        WE ACCEPT
+      </h5>
+      <a href="#">
+        <img src="resource/img/icons/icon-pay-01.png" alt="ICON-PAY">
+      </a>
+
+      <a href="#">
+        <img src="resource/img/icons/icon-pay-02.png" alt="ICON-PAY">
+      </a>
+
+      <a href="#">
+        <img src="resource/img/icons/icon-pay-03.png" alt="ICON-PAY">
+      </a>
+
+      <a href="#">
+        <img src="resource/img/icons/icon-pay-04.png" alt="ICON-PAY">
+      </a>
+
+      <a href="#">
+        <img src="resource/img/icons/icon-pay-05.png" alt="ICON-PAY">
+      </a>
+    </div>
+  </footer>
+
+    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+  </body>
 </html>
