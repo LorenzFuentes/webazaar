b8df571a26fcde2aaeb8b1eb14e230f2d1509714
LorenzFuentes <fuentes2231163@mls.ceu.edu.ph>
2023-05-02 00:12:27 +0800

    Updated deisgn of register page

--- index.php
+++ index.php
@@ -48,6 +48,15 @@
             <li class="nav-item pr-3 ">
               <a class="nav-link text-light ">CONTACT</a>
             </li>
+            <li class="nav-item dropdown pr-3 ">
+              <a class="nav-link dropdown-toggle text-light " href="#" role="button" data-toggle="dropdown" aria-expanded="false">SETTINGS</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="updateprofile.php">UPDATE PROFILE</a>
+                <a class="dropdown-item" href="changepassword.php">UPDATE PASSWORD</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">LOG-OUT</a>
+              </div>
+            </li>
           </ul>
         </div>
       </nav>
